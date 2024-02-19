091119dd3fb0dc6dd9ef9d7ff452321a497346cd
code2828 <stevenhaocheng@163.com>
2024-02-19 20:23:51 +0800

    found a bug

--- PhBr.cpp
+++ PhBr.cpp
@@ -24,7 +24,7 @@ using json = nlohmann::json;
 
 json CR100J = {
 	{"ID", "CR100J"},
-	{"maximum_acceleration", 0.5},
+	{"maximum_acceleration", 1000},
 	{"maximum_velocity", 28}
 };
 
@@ -107,15 +107,20 @@ json j = {
 	}}}
 };
 
-decimal _basic_velocity(decimal tt, decimal len, decimal a_max, decimal v_max, json jj=j) // tt in time_units, result in meter per time_unit assuming a=1
+// debug
+decimal global_v_cap=-8354.12347;
+
+decimal _basic_velocity(decimal tt, decimal len, decimal v_max) // tt in time_units, result in meter per time_unit assuming a=1
 {
 	if(tt<0)throw("wth u tried to use a negative time!");
+	cerr<<"\t\ttt="<<tt<<endl;
 	decimal v_cap = (-3.0+sqrt(9.0+4*len))/2.0;
 	v_max=min(v_max,v_cap);
+		global_v_cap=v_cap;
 	decimal ta=v_max+1.00, m=ta+3.00, n=m-4.00, tall=(len-2.0*(0.50*v_max*v_max+v_max))/v_max+2*ta+1.00, tsym=tall-ta+m-1.00;
 	if(tsym-tt<tt)tt=tsym-tt;
 	decimal ret=0;
-	if(0<=tt&&tt<=1)ret=0;
+	if(tt<=1)ret=0;
 	else if(1<tt&&tt<=2)ret=tt*tt*tt/6.0-0.5*tt*tt+0.5*tt-1/6.0;
 	else if(2<tt&&tt<=3)ret=0-tt*tt*tt/6.0+1.5*tt*tt-3.5*tt+2.5;
 	else if(3<tt&&tt<=m-3)ret=tt-2;
@@ -130,16 +135,17 @@ decimal velocity(decimal t, long interval_start, long interval_end, json train, 
 	long interval_length=j_["stations"][interval_end].value("meter",0)-j_["stations"][interval_start].value("meter",0);
 	decimal tu=j_.value("time_unit",0);
 	decimal a_max=train.value("maximum_acceleration",0.0)*tu*tu;
-	return _basic_velocity(t/tu,interval_length/a_max,a_max,train.value("maximum_velocity",0.0)*tu/a_max,j_)*a_max/tu;
+	decimal v_max=train.value("maximum_velocity",0.0)*tu;
+	return _basic_velocity(t/tu,interval_length/a_max,v_max/a_max)*a_max/tu;
 }
 
 int main()
 {
 	for(decimal d=0;d<=1000;d+=1)
 	{
-		cout<<"At time "<<d<<"s, the train's velocity is "<<velocity(d,0,3,CR100J)<<"m/s\n";
+		cout<<"At time "<<d<<"s, the train's velocity is "<<velocity(d,0,3,CR100J)<<"m/s.\n"
 	}
 
 	return 0;
 }
-
+// 最大加速度不可过大，因若然则未达到最大加速度前即达最大速度，bug！
