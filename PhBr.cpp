d25fc9e2ac3997b632e54cd5450fb223b87a16ea
code2828 <stevenhaocheng@163.com>
2024-03-30 22:22:31 +0800

    rework code

--- PhBr.cpp
+++ PhBr.cpp
@@ -16,6 +16,8 @@
  */
 #include<iostream>
 #include<iomanip>
+#include<vector>
+#include<fstream>
 #include<cmath>
 #include"json.hpp"
 #define decimal long double
@@ -23,156 +25,32 @@
 using namespace std;
 using json = nlohmann::json;
 
-json CR1J = { 
-    {"ID", "CR1J"},
-    {"maximum_acceleration", 0.2},
-    {"maximum_velocity", 0.278}
+struct Dir
+{
+	int serial;
+	string name;
+	int nex;
+	int nex_dir;
+	int dis;
 };
 
-json CR10J = {
-    {"ID", "CR10J"},
-    {"maximum_acceleration", 0.3},
-    {"maximum_velocity", 3}
+struct Station
+{
+	int serial;
+	string id;
+	int dir_num;
+	Dir dirs[100];
+	char dir_comp[100][100] = {""};
 };
 
-json CR200J = {
-	{"ID", "CR200J"},
-	{"maximum_acceleration", 0.41},
-	{"maximum_velocity", 34.3}
-};
-
-json CR400AF = {
-    {"ID", "CR400AF"},
-    {"maximum_acceleration", 0.53},
-    {"maximum_velocity", 100}
-};
-
-json CR1000Z = {
-    {"ID", "CR1000Z"},
-    {"maximum_acceleration", 1},
-    {"maximum_velocity", 277.78}
-};
-
-json CRNAN = {
-    {"ID", "CRNAN"},
-    {"maximum_acceleration", 100},
-    {"maximum_velocity", 100000}
-};
-
-json j = {
-	{"ID", -1},
-	{"name", "random"},
-	{"stations", {{
-		{"name", "长亘"},
-        {"platforms",1},
-        {"meter",0}
-    },{
-		{"name", "山城"},
-        {"platforms",1},
-        {"meter",10000}
-    },{
-		{"name", "高粱"},
-        {"platforms",1},
-        {"meter",15000}
-    },{
-		{"name", "黄恩东"},
-        {"platforms",1},
-        {"meter",20000}
-    },{
-		{"name", "开阳"},
-        {"platforms",1},
-        {"meter",34000}
-    },{
-		{"name", "白市西"},
-        {"platforms",1},
-        {"meter",45000}
-    },{
-		{"name", "繁城"},
-        {"platforms",1},
-        {"meter",51000}
-    },{
-		{"name", "凉水江西"},
-        {"platforms",1},
-        {"meter",58091}
-    },{
-		{"name", "朝栾西"},
-        {"platforms",1},
-        {"meter",69819}
-    },{
-		{"name", "冷爱南"},
-        {"platforms",1},
-        {"meter",78822}
-    },{
-		{"name", "北城"},
-        {"platforms",1},
-        {"meter",87324}
-    },{
-		{"name", "氢北南"},
-        {"platforms",1},
-        {"meter",93929}
-    },{
-		{"name", "南城"},
-	    {"platforms",1},
-	    {"meter",97182}
-	},{
-		{"name", "广台东"},
-		{"platforms",1},
-		{"meter",102434}
-	},{
-		{"name", "艳隆西"},
-		{"platforms",1},
-		{"meter",112343}
-	},{
-		{"name", "平佥龙骨"},
-		{"platforms",1},
-		{"meter",122516}
-	},{
-		{"name", "龙驿南川"},
-		{"platforms",1},
-		{"meter",135670}
-	},{
-		{"name", "千兰北"},
-		{"platforms",1},
-		{"meter",150067}
-	},{
-		{"name", "随机市北"},
-        {"platforms",1},
-        {"meter",8127383}
-	}}}
-};
-
-json jingjiu = {
-    {"ID", 2},
-    {"name", "jingjiuxian"},
-    {"stations", {{
-        {"name", "北京西"},
-        {"platforms",1},
-        {"meter",0}
-    },{
-        {"name", "任丘"},
-        {"platforms",1},
-        {"meter",147000}
-    },{
-        {"name", "衡水"},
-        {"platforms",1},
-        {"meter",274000}
-    },{
-        {"name", "九江"},
-        {"platforms",1},
-        {"meter",1314000}
-    },{
-        {"name", "南昌"},
-        {"platforms",1},
-        {"meter",1449000}
-	}}}
-};
+vector<Station> v;
 
 decimal _basic_v(decimal x, decimal x00, decimal y00)
 {
 	return y00/2*sin(PI/x00*x-PI/2)+y00/2;
 }
 
-decimal v(decimal t, json tr, int len)
+decimal vv(decimal t, json tr, int len)
 {
 	decimal y00=tr.value("maximum_velocity",0.0);
 	y00=min(y00,len/(decimal)2.0);
@@ -203,12 +81,76 @@ decimal place(decimal t, json tr, int len, bool only_get_time=false)
 	else return y00*(t-x00/2.0);
 }
 
+void read_stations()
+{
+	ifstream fin("dat/reality.stationlist");
+	char c;
+	while(fin.get(c))
+	{
+		if(c=='@')
+		{
+			Station new_stat;
+			fin>>new_stat.serial>>new_stat.id>>new_stat.dir_num;
+			for(int i=0;i<new_stat.dir_num;i++)
+			{
+				fin>>new_stat.dirs[i].serial>>new_stat.dirs[i].name>>new_stat.dirs[i].nex>>new_stat.dirs[i].nex_dir>>new_stat.dirs[i].dis;
+				if(i!=new_stat.dirs[i].serial)throw("Serial number does not match index!!\n");
+			}
+			for(int j=0;j<new_stat.dir_num;j++)
+			{
+				for(int k=0;k<new_stat.dir_num;k++)
+				{
+					fin>>new_stat.dir_comp[j][k];
+				}
+			}
+			v.push_back(new_stat);
+		}
+	}
+}
+
+bool used[100]={0};
+string find_path(int st, int en)
+{
+	if(st>=v.size())
+	{
+		cout<<"st is too big!\n";
+		return"";
+	}
+	if(en>=v.size())
+	{
+		cout<<"en is too big!\n";
+		return "";
+	}
+	cout<<v[st].id<<"到"<<v[en].id<<endl;
+	if(st==en)
+	{
+		cout<<"=====>\t好！\n";
+		return v[st].id;
+	}
+	used[st]=1;
+	for(int i=0;i<v[st].dir_num;i++)
+	{
+		if(v[st].dirs[i].dis>0 && !used[v[st].dirs[i].nex])
+		{
+					string ret=find_path(v[st].dirs[i].nex,en);
+			if(ret!="")
+			{
+				ret.insert(0,"->");
+				ret.insert(0,v[st].id);
+				return ret;
+			}
+		}
+	}
+	used[st]=0;
+	return "";
+}
+
+/*
 void generate_simple_timetable(json line, json tr, pair<int,int> starting_time)
 {
 	pair<int,int> curtime=starting_time;int day=0;
     int last_meter=0;
-    cout<<"越站几率？（0～100）  "<<flush;
-    int quang=0;cin>>quang;
+    int quang=73;
     for(json::iterator it=line["stations"].begin(); it!=line["stations"].end(); it++)
     {
         if(rand()%100<quang&&it!=line["stations"].begin()&&(it+1)!=line["stations"].end())continue;
@@ -226,13 +168,13 @@ void generate_simple_timetable(json line, json tr, pair<int,int> starting_time)
         cout<<'+'<<day<<' '<<curtime.first<<':'<<curtime.second<<'\n';
         last_meter=(*it).value("meter",0);
     }
-}
+}*/
 
 int main()
 {
 	srand((unsigned)time(0));
-	generate_simple_timetable(jingjiu,CR200J,pair<int,int>(19,59));
-	generate_simple_timetable(j,CRNAN,pair<int,int>(8,0));
-	
+	read_stations();
+	cout<<find_path(1,4)<<endl;
+
 	return 0;
 }
